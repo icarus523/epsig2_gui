494154ffb1213cb8f67ac6e0b219f1110acdc2bf
icarus523 <icarus523@users.noreply.github.com>
2023-01-25 16:11:35 +1000

    Update epsig2_gui.py
    Updated Version to 2.2.2
    Add comment details

--- epsig2_gui.py
+++ epsig2_gui.py
@@ -68,6 +68,7 @@
 #       - minor gui fixes (sizing, text wrapping, etc.)
 # v2.2.1 - Added fix for displaying results when hashing .BIN files when the option to use epsig.exe is selected
 #       - Added a message in the log, that using epsig.exe to hash .BIN files is not supported, and that epsig2.py will be used instead.
+# v2.2.2 - Added fix for displaying results when utilising epsig.exe, that espig2.format_output won't lstrip 0x or 0X, but still zfill(40) or zfill(64)
 import os
 import sys
 import csv
@@ -103,7 +104,7 @@ from queue import Queue, Empty
 
 from epsig2 import epsig2, CacheFile, BNKEntry, Seed, DEFAULT_CACHE_FILE
 
-VERSION = "2.2.1"
+VERSION = "2.2.2"
 
 EPSIG_LOGFILE = "epsig2.log"
 MAXIMUM_BLOCKSIZE_TO_READ = 65535
